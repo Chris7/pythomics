585b6ccbd6291edaddaab91a69e1f198a24fea07
Chris Mitchell <cmitch48@live.johnshopkins.edu>
2014-01-16 08:42:36 -0500

    Modified fastadigest to accept stdin and stdout for piping

--- pythomics/scripts/fastadigest.py
+++ pythomics/scripts/fastadigest.py
@@ -7,8 +7,8 @@ import pythomics.parsers.fasta as fasta
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--enzyme', help="The enzyme to cleave with.", choices=config.ENZYMES.keys(), type=str, default='trypsin')
-parser.add_argument('-f', '--file', help="The fasta file to cleave.", type=str, required=True)
-parser.add_argument('-o', '--out', help="The file to write digested products to.", type=str, required=True)
+parser.add_argument('-f', '--file', nargs='?', help="The fasta file to cleave.", type=argparse.FileType('r'), default=sys.stdin)
+parser.add_argument('-o', '--out', nargs='?', help="The file to write digested products to.", type=argparse.FileType('w'), default=sys.stdout)
 parser.add_argument('-t', '--type', help="The type of fasta file (default protein).", choices=['prot','nt'], type=str, default='prot')
 parser.add_argument('--frame', help="If using a nucleotide file, translate in how many frames?", choices=[1,3,6], type=int)
 parser.add_argument('--min', help="Minimum cleavage length", type=int, default=7)
@@ -26,20 +26,17 @@ def main():
         digest_frame = 3
     digest_min = args.min
     digest_max = args.max
-    if not os.path.exists(file_name):
-        print "File does not exist."
-        return 1
     if digest_type == 'prot' and digest_frame:
         print "Protein digestions cannot have a frame."
         return 1
     if digest_type == 'nt' and not digest_frame:
         print "Nucleotide digestions must specify the frame."
         return 1
-    fasta = fasta.FastaIterator(file_name)
+    fasta_file = fasta.FastaIterator(file_name)
     enzyme = digest.Enzyme( enzyme=enzyme_choice )
-    with open(args.out, 'wb') as o:
+    with args.out as o:
         if digest_type == 'nt':
-            for header, sequence in fasta:
+            for header, sequence in fasta_file:
                 for i in xrange(digest_frame):
                     strand='+'
                     for protein_index,protein_sequence in enumerate(fasta._translate(sequence[i:]).split('*')):
@@ -53,7 +50,7 @@ def main():
                             for peptide_index,peptide in enumerate(peptides):
                                 o.write('>%s F:%s%d Orf:%d Pep:%d \n%s\n' % (header,strand,i+1,protein_index+1,peptide_index+1,peptide))
         else:
-            for header, sequence in fasta:
+            for header, sequence in fasta_file:
                 peptides = enzyme.cleave(sequence, min=digest_min, max=digest_max)
                 for peptide_index,peptide in enumerate(peptides):
                     o.write('>%s Pep:%d \n%s\n' % (header,peptide_index+1,peptide))
