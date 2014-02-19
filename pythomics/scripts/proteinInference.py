d0c8b160f4474c966866ddcb9a67720a7946ad33
Chris Mitchell <cmitch48@live.johnshopkins.edu>
2014-02-19 12:08:59 -0500

    Major changes to proteinInference: Peptides are grouped together instead of reported as found in spectra files. iBAQ formula fixed again. Case sensitivity option added.

--- pythomics/scripts/proteinInference.py
+++ pythomics/scripts/proteinInference.py
@@ -3,9 +3,9 @@
 __author__ = 'chris'
 
 description = """
-This script will annotate a tab delimited text file with which protein headers
-correspond to peptides present and can also use this annotation file to provide
-iBAQ measures.
+This script will annotate a tab delimited text file with peptides with
+corresponding proteins present in an annotation file, and can also
+use this annotation to include iBAQ measures.
 """
 
 import argparse, sys, re, csv, copy, decimal
@@ -29,6 +29,7 @@ group.add_argument('--enzyme', help="The enzyme to cleave with.", choices=config
 group.add_argument('--min', help="Minimum cleavage length", type=int, default=7)
 group.add_argument('--max', help="Maximum cleavage length", type=int, default=30)
 group.add_argument('--no-normalize', help="Don't normalize iBAQ to total intensity", action='store_false', default=True)
+group.add_argument('--case-sensitive', help="Treat peptides as case-sensitive (ie group modifications)", action='store_true', default=False)
 
 def main():
     args = parser.parse_args()
@@ -43,6 +44,7 @@ def main():
     sys.stderr.write("Reading in Fasta file.\n")
     normalize = args.no_normalize
     ibaq = args.ibaq
+    case_sens = args.case_sensitive
     if ibaq:
         precursor_columns = [int(i) for i in args.precursors.split(',')] if args.precursors else None
         enzyme = digest.Enzyme( enzyme=args.enzyme )
@@ -52,51 +54,81 @@ def main():
         regex = re.compile(args.regex)
         fasta_headers = [regex.search(header) for header in fasta_headers]
         protein_sequences = [protein_sequences[i] for i, v in enumerate(fasta_headers) if v]
-        sys.stderr.write("%d header sequences did not match regex and have been discarded." % (len(fasta_headers)-len(protein_sequences)))
+        sys.stderr.write("%d header sequences did not match regex and have been discarded.\n" % (len(fasta_headers)-len(protein_sequences)))
         fasta_headers = [' '.join(i.groups()) for i in fasta_headers if i]
     if ibaq:
         ibaq_protein_sequence = copy.deepcopy(protein_sequences)
+        cleaved = {}
     protein_sequences = '\n'.join(protein_sequences)
+    peptide_history = {}
     with args.out as o:
         writer = csv.writer(o, delimiter=delimiter)
+        header = ['Peptide', 'PSMS', 'Total Precursor Area']
+        if inference:
+            header.append('Proteins')
+        if ibaq:
+            if normalize:
+                header.append('Normalized Precursor Intensity')
+            header.append('iBAQ')
+        writer.writerow(header)
         with tsv_file as f:
             reader = csv.reader(f, delimiter=delimiter)
             for line_num, entry in enumerate(reader):
-                if line_num < header_lines:#we always assume we have 1 header line
-                    if inference:
-                        entry.append('Proteins')
+                if line_num < header_lines:#we assume the first header line is the one we care about
                     if ibaq:
-                        entry.append('iBAQ')
                         if not precursor_columns:
                             precursor_columns = [i for i, v in enumerate(entry) if 'precursor' in v.lower()]
                         if normalize:
                             normalizations = [0 for i in precursor_columns]
-                            for entry in reader:
-                                for n_i,e_i in enumerate(precursor_columns):
-                                    if entry[e_i]:
-                                        normalizations[n_i] += decimal.Decimal(entry[e_i])
-                            f.seek(0)
-                            reader.next()
-                        else:
-                            normalizations = [1 for i in precursor_columns]
-
                 else:
-                    peptide = entry[peptide_column].upper()
-                    sites = [match.start() for match in re.finditer(peptide, protein_sequences)]
-                    indices = [protein_sequences.count('\n', 0, match_position) for match_position in sites]
-                    matches = ';'.join([fasta_headers[i] for i in indices])
-                    if inference:
-                        entry.append(matches)
+                    peptide = entry[peptide_column]
+                    if not case_sens:
+                        peptide = peptide.upper()
                     if ibaq:
-                        ibaqs = []
-                        for protein_index in indices:
-                            peptides = len(enzyme.cleave(ibaq_protein_sequence[protein_index], min=digest_min, max=digest_max))
-                            if not peptides:
-                                ibaqs.append('NA')
-                                continue
-                            ibaqs.append(float(sum([decimal.Decimal(entry[e_i])/normalizations[n_i] if entry[e_i] else 0.0 for n_i,e_i in enumerate(precursor_columns)])/peptides))
-                        entry.append(';'.join(str(i) for i in ibaqs))
-                writer.writerow(entry)
+                        for n_i, e_i in enumerate(precursor_columns):
+                            if entry[e_i]:
+                                intensity = decimal.Decimal(entry[e_i])
+                                try:
+                                    peptide_history[peptide][n_i].add(intensity)
+                                except KeyError:
+                                    peptide_history[peptide] = {}
+                                    for i in xrange(len(precursor_columns)):
+                                        peptide_history[peptide][i] = set([])
+                                    peptide_history[peptide][n_i].add(intensity)
+        if normalize:
+            for peptide in peptide_history:
+                for i, v in peptide_history[peptide].iteritems():
+                    normalizations[i] += sum(v)
+        else:
+            normalizations = [1 for peptide in peptide_history for intensities in peptide_history[peptide]]
+        for index, (peptide, d) in enumerate(peptide_history.iteritems()):
+            if not index%1000:
+                sys.stderr.write('%d of %d complete.\n' % (index, len(peptide_history)))
+            precursor_int = float(sum([sum(d[i]) for i in d]))
+            entry = [peptide, sum([len(d[i]) for i in d]), precursor_int]
+            if inference or ibaq:
+                sites = [match.start() for match in re.finditer(peptide, protein_sequences)]
+                indices = [protein_sequences.count('\n', 0, match_position) for match_position in sites]
+            if inference:
+                matches = ';'.join([fasta_headers[i] for i in indices])
+                entry.append(matches)
+            if ibaq:
+                ibaqs = []
+                intensities = [sum(d[i]) for i in d]
+                if normalize:
+                    precursor_int = sum([intensities[i]/normalizations[i] for i in xrange(len(normalizations))])
+                    entry.append(precursor_int)
+                for protein_index in indices:
+                    peptides = cleaved.get(protein_index,None)
+                    if peptides is None:
+                        peptides = len(enzyme.cleave(ibaq_protein_sequence[protein_index], min=digest_min, max=digest_max))
+                        cleaved[protein_index] = peptides
+                    if not peptides:
+                        ibaqs.append('NA')
+                        continue
+                    ibaqs.append(precursor_int/peptides)
+                entry.append(';'.join(str(i) for i in ibaqs))
+            writer.writerow(entry)
 
 if __name__ == "__main__":
     sys.exit(main())
