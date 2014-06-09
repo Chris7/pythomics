225bc9684438ae5a8d33a30453e8db80d4768d1f
Chris Mitchell <cmitch48@live.johnshopkins.edu>
2014-06-09 08:33:58 -0400

    version increase for minor fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pythomics',
-      version='0.1.83',
+      version='0.1.84',
       description='A multi-omic python package',
       url='https://github.com/pandeylab/pythomics',
       author='Chris Mitchell',
