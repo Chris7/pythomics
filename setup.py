e16b1ab2bea715f581d5c00c27f4c6ec4f510d55
Chris Mitchell <cmitch48@live.johnshopkins.edu>
2015-02-03 15:31:16 -0500

    version increase, add scripts to setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pythomics',
-      version='0.1.95',
+      version='0.1.96',
       description='A multi-omic python package',
       url='https://github.com/pandeylab/pythomics',
       author='Chris Mitchell',
@@ -12,5 +12,6 @@ setup(name='pythomics',
       scripts=['pythomics/scripts/fastadigest.py', 'pythomics/scripts/fastadigeststats.py',
                'pythomics/scripts/incorporateVCF.py', 'pythomics/scripts/fetchOrfs.py',
                'pythomics/scripts/incorporateGFF.py', 'pythomics/scripts/proteinInference.py',
-               'pythomics/scripts/featureCollapser.py',],
+               'pythomics/scripts/featureCollapser.py', 'pythomics/scripts/fastxTrimmer.py', 
+               'pythomics/scripts/intersectFiles.py', 'pythomics/scripts/junctionalReads.py',],
       zip_safe=False)
