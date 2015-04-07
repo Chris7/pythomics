0e0b388b92d626374e7048473285eeac159cbc99
Chris Mitchell <cmitch48@live.johnshopkins.edu>
2015-04-07 13:18:24 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pythomics',
-      version='0.1.99',
+      version='0.2.00',
       description='A multi-omic python package',
       url='https://github.com/pandeylab/pythomics',
       author='Chris Mitchell',
