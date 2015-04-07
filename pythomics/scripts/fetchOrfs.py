4b2678e98a5583a695c6c4c71e269e48003dd962
Chris Mitchell <cmitch48@live.johnshopkins.edu>
2015-04-07 13:17:53 -0400

    option to keep leading AAs from mets added, fixed negative strand to not look for just Mets but have stop-to-stop as well

--- pythomics/scripts/fetchOrfs.py
+++ pythomics/scripts/fetchOrfs.py
@@ -19,6 +19,7 @@ parser.add_argument('--min', help="Minimum ORF length in amino acids.", type=int
 parser.add_argument('--both-strands', help="Search both strands for ORFs.", action='store_true')
 parser.add_argument('--no-met-start', help="Output ORFs starting with amino acids other than MET", action='store_true')
 parser.add_argument('--from-met', help="Truncate leading amino acids up to MET", action='store_true')
+parser.add_argument('--from-met-keep', help="Truncate leading amino acids up to MET, but keep the untruncated version as well.", action='store_true')
 
 def main():
     args = parser.parse_args()
@@ -28,6 +29,7 @@ def main():
     negative_strand = args.both_strands
     no_met = args.no_met_start
     from_met = args.from_met
+    from_met_keep = args.from_met_keep
     with args.out as o:
         for header, sequence in fasta_file:
             for i in xrange(3):
@@ -35,23 +37,34 @@ def main():
                 translation = fasta._translate(sequence[i:])
                 translation = translation.split('*')
                 for protein_index,protein_sequence in enumerate(translation):
+                    protein_sequences = [protein_sequence]
                     if from_met:
+                        if not from_met_keep:
+                            protein_sequences = []
                         pos = protein_sequence.find('M')
                         if pos == -1:
-                            continue
-                        protein_sequence = protein_sequence[pos:]
-                    if len(protein_sequence) >= orf_min and (no_met or protein_sequence[0] == 'M'):
-                        o.write('>%s F:%s%d Orf:%d\n%s\n' % (header,strand,i+1,protein_index+1,protein_sequence))
+                            if not from_met_keep:
+                                continue
+                        protein_sequences.append(protein_sequence[pos:])
+                    for protein_sequence in protein_sequences:
+                        if len(protein_sequence) >= orf_min and (no_met or protein_sequence[0] == 'M'):
+                            o.write('>%s F:%s%d Orf:%d\n%s\n' % (header,strand,i+1,protein_index+1,protein_sequence))
                 if negative_strand:
                     strand = '-'
                     translation = fasta._translate(fasta._reverse_complement(sequence)[i:])
                     for protein_index,protein_sequence in enumerate(translation):
-                        pos = protein_sequence.find('M')
-                        if pos == -1:
-                            continue
-                        protein_sequence = protein_sequence[pos:]
-                        if len(protein_sequence) >= orf_min and (no_met or protein_sequence[0] == 'M'):
-                            o.write('>%s F:%s%d Orf:%d\n%s\n' % (header,strand,i+1,protein_index+1,protein_sequence))
+                        protein_sequences = [protein_sequence]
+                        if from_met:
+                            if not from_met_keep:
+                                protein_sequences = []
+                            pos = protein_sequence.find('M')
+                            if pos == -1:
+                                if not from_met_keep:
+                                    continue
+                            protein_sequences.append(protein_sequence[pos:])
+                        for protein_sequence in protein_sequences:
+                            if len(protein_sequence) >= orf_min and (no_met or protein_sequence[0] == 'M'):
+                                o.write('>%s F:%s%d Orf:%d\n%s\n' % (header,strand,i+1,protein_index+1,protein_sequence))
 
 if __name__ == "__main__":
     sys.exit(main())
