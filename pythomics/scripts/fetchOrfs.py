fba160502d543a757d261923c5caca5778c2a2ae
Chris Mitchell <cmitch48@live.johnshopkins.edu>
2015-04-07 13:31:48 -0400

    fix for double header from from-met-keep

--- pythomics/scripts/fetchOrfs.py
+++ pythomics/scripts/fetchOrfs.py
@@ -21,6 +21,7 @@ parser.add_argument('--no-met-start', help="Output ORFs starting with amino acid
 parser.add_argument('--from-met', help="Truncate leading amino acids up to MET", action='store_true')
 parser.add_argument('--from-met-keep', help="Truncate leading amino acids up to MET, but keep the untruncated version as well.", action='store_true')
 
+
 def main():
     args = parser.parse_args()
     file_name = args.fasta
@@ -30,6 +31,22 @@ def main():
     no_met = args.no_met_start
     from_met = args.from_met
     from_met_keep = args.from_met_keep
+    if from_met_keep:
+        from_met = True
+        no_met = True
+    def write_sequence(handle, header, protein_index, protein_sequence):
+        header1 = '>%s F:%s%d Orf:%d' % (header,strand,i+1,protein_index+1)
+        protein_sequences = [(header1, protein_sequence)]
+        if from_met:
+            pos = protein_sequence.find('M')
+            if pos == -1:
+                return
+            header2 = '>%s(%d upstream removed) F:%s%d Orf:%d' % (header,pos, strand,i+1,protein_index+1)
+            protein_sequences.append((header2, protein_sequence[pos:]))
+        for protein_header, protein_sequence in protein_sequences:
+            if len(protein_sequence) >= orf_min and (no_met or protein_sequence[0] == 'M'):
+                handle.write('%s\n%s\n' % (protein_header, protein_sequence))
+
     with args.out as o:
         for header, sequence in fasta_file:
             for i in xrange(3):
@@ -37,34 +54,11 @@ def main():
                 translation = fasta._translate(sequence[i:])
                 translation = translation.split('*')
                 for protein_index,protein_sequence in enumerate(translation):
-                    protein_sequences = [protein_sequence]
-                    if from_met:
-                        if not from_met_keep:
-                            protein_sequences = []
-                        pos = protein_sequence.find('M')
-                        if pos == -1:
-                            if not from_met_keep:
-                                continue
-                        protein_sequences.append(protein_sequence[pos:])
-                    for protein_sequence in protein_sequences:
-                        if len(protein_sequence) >= orf_min and (no_met or protein_sequence[0] == 'M'):
-                            o.write('>%s F:%s%d Orf:%d\n%s\n' % (header,strand,i+1,protein_index+1,protein_sequence))
+                    write_sequence(o, header, protein_index, protein_sequence)
                 if negative_strand:
                     strand = '-'
                     translation = fasta._translate(fasta._reverse_complement(sequence)[i:])
                     for protein_index,protein_sequence in enumerate(translation):
-                        protein_sequences = [protein_sequence]
-                        if from_met:
-                            if not from_met_keep:
-                                protein_sequences = []
-                            pos = protein_sequence.find('M')
-                            if pos == -1:
-                                if not from_met_keep:
-                                    continue
-                            protein_sequences.append(protein_sequence[pos:])
-                        for protein_sequence in protein_sequences:
-                            if len(protein_sequence) >= orf_min and (no_met or protein_sequence[0] == 'M'):
-                                o.write('>%s F:%s%d Orf:%d\n%s\n' % (header,strand,i+1,protein_index+1,protein_sequence))
-
+                        write_sequence(o, header, protein_index, protein_sequence)
 if __name__ == "__main__":
     sys.exit(main())
