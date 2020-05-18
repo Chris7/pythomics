9b4a9789c0701b3d3e5bd12053aac958536666bf
Chris Mitchell <chris.mit7@gmail.com>
2020-05-18 11:54:41 -0400

    Windows hndle

--- tests/test_mzml_parser.py
+++ tests/test_mzml_parser.py
@@ -15,8 +15,10 @@ class TestMZMLWriter(FixtureMixin, unittest.TestCase):
 
     def test_writes_mzml_scans(self):
         parser = MZMLIterator(open(self.mzml, "rb"))
-        with tempfile.NamedTemporaryFile(mode="wb") as tf:
+        with tempfile.NamedTemporaryFile(delete=False, mode="wb") as tf:
+            self.addCleanup(os.remove, tf.name)
             parser.writeScans(handle=tf, scans=["5", "6"])
-            new_mzml = MZMLIterator(open(tf.name, "rb"))
-            scan_ids = [i.id for i in new_mzml if i]
-            self.assertEqual(scan_ids, ["5", "6"])
+
+        new_mzml = MZMLIterator(open(tf.name, "rb"))
+        scan_ids = [i.id for i in new_mzml if i]
+        self.assertEqual(scan_ids, ["5", "6"])
