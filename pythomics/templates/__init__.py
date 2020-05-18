a2823cc68a0f79ea037de1af0968e1bb01621043
Chris Mitchell <chris.mit7@gmail.com>
2020-05-17 22:02:56 -0400

    More fixing of past garbage

--- pythomics/templates/__init__.py
+++ pythomics/templates/__init__.py
@@ -18,22 +18,27 @@ class GenericIterator(six.Iterator):
     def __init__(self, filename, delimiter="\n", *args, **kwargs):
         self.delimiter = delimiter
         self.filename = filename
-        if isinstance(filename, six.string_types) and filename.endswith(".gz"):
-            self.gzip = True
-            self.handle = gzip.GzipFile(filename, mode="rb")
-        elif isinstance(filename, six.string_types):
-            self.handle = open(filename, "rb")
-        elif (six.PY3 and isinstance(filename, IOBase)) or (
+        if (six.PY3 and isinstance(filename, IOBase)) or (
             six.PY2 and isinstance(filename, file)
         ):
             self.filename = filename.name
-            if filename.name.endswith(".gz"):
+            self._handle = filename
+
+    @property
+    def handle(self):
+        handle = getattr(self, "_handle", None)
+
+        if not handle:
+            filename = self.filename
+            if isinstance(filename, six.string_types) and filename.endswith(".gz"):
                 self.gzip = True
-                self.handle = gzip.GzipFile(filename.name, mode="rb")
-            else:
-                self.handle = filename
-        else:
-            raise TypeError
+                handle = gzip.GzipFile(filename, mode="rb")
+            elif isinstance(filename, six.string_types):
+                handle = open(filename, "rb")
+
+            self._handle = handle
+
+        return handle
 
     def __iter__(self):
         return self
@@ -42,7 +47,7 @@ class GenericIterator(six.Iterator):
         if self.gzip:
             if self.contents:
                 return self.contents.popleft()
-            new_contents = self.filename.read(self.CHUNK_SIZE)
+            new_contents = self.handle.read(self.CHUNK_SIZE)
             if not new_contents:
                 if self.UNCONSUMED:
                     return self.UNCONSUMED
@@ -61,7 +66,7 @@ class GenericIterator(six.Iterator):
             if self.contents:
                 return self.contents.popleft()
         else:
-            return six.next(self.filename).strip()
+            return six.next(self.handle).strip()
 
     def __next__(self):
         return self._next()
--- tests/test_fasta_iterator.py
+++ tests/test_fasta_iterator.py
@@ -8,11 +8,11 @@ class Test_Fasta_Iterator(unittest.TestCase):
     def setUp(self):
         base_dir = os.path.split(__file__)[0]
         data_dir = os.path.join(base_dir, "fixtures")
-        self.handle = os.path.join(data_dir, "test_fasta_iterator.fasta")
-        self.index = "{}.{}".format(self.handle, "fai")
+        self.fasta_file = os.path.join(data_dir, "test_fasta_iterator.fasta")
+        self.index = "{}.{}".format(self.fasta_file, "fai")
 
     def test_fasta_iterator(self):
-        f = parser.FastaIterator(self.handle, delimiter=">")
+        f = parser.FastaIterator(self.fasta_file, delimiter=">")
         assert isinstance(f, parser.FastaIterator)
         headers = []
         sequences = []
@@ -26,15 +26,13 @@ class Test_Fasta_Iterator(unittest.TestCase):
         )
 
     def test_fasta_get_sequence(self):
-        f = parser.FastaIterator(self.handle, index=self.index)
+        f = parser.FastaIterator(self.fasta_file)
         out = f.get_sequence("c1", 5, 30)
-        self.assertEqual(
-            "DDDKIVGGYTCAANSIPYQVSLNSGS", out, "Fasta get_sequence #1 Failure"
-        )
+        self.assertEqual("DDDKIVGGYTCAANSIPYQVSLNSGS", out)
         f.handle.close()
 
     def test_fasta_index_build(self):
-        f = parser.FastaIterator(self.handle)
+        f = parser.FastaIterator(self.fasta_file)
         f.build_fasta_index()
         index = open(self.index, "r").read()
         self.assertEqual(
